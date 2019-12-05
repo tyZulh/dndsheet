4958f05634d391212c08ecc6e02a29fdb5972f5b
Thomas Paoli <thomas.paoli.tp@gmail.com>
2019-12-05 17:49:00 +0100

    send data from headerItem to header

--- character_sheet/src/components/Header.js
+++ character_sheet/src/components/Header.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import HeaderItem from './HeaderItem'
 import './Header.scss'
 
 const Header = () => {
+  const [characterPersonalData, setPersonalData] =  useState({})
   const items = [
     'Personnage',
     'Classes',
@@ -12,10 +13,32 @@ const Header = () => {
     'Taille',
     'Poid',
   ]
+
+  const handlePersonalData = (key, value) => {
+    console.log(characterPersonalData);
+    
+    console.log('key ', key, ', value ', value);
+    // if(!characterPersonalData){
+    //   return
+    // }
+    const dataTmp = characterPersonalData
+    Object.assign(dataTmp, {key: value})
+    console.log(dataTmp);
+    
+    
+    setPersonalData(dataTmp)
+    
+  }
+
+  useEffect(()=>{
+    console.log(characterPersonalData);
+    
+  })
+
   return(
     <header className='header-wrapper'>
       {items.map(item => {
-          return (<HeaderItem title={item}></HeaderItem>)
+          return (<HeaderItem title={item} handleData={handlePersonalData} value={characterPersonalData ? characterPersonalData.value : ''}></HeaderItem>)
         })}
     </header>
   )
