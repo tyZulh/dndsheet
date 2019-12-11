7d90cc970c565b2e930a71d72c61f247534c1192
Thomas Paoli <thomas.paoli.tp@gmail.com>
2019-12-11 17:54:20 +0100

    add the Characteristics component

--- character_sheet/src/App.js
+++ character_sheet/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header'
+import Characteristics from './components/Characteristics'
 import './App.css';
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
+      <Characteristics />
     </div>
   );
 }
---
+++ character_sheet/src/components/Characteristics.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Characteristics.scss'
+
+const Characteristics = () => {
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default Characteristics
